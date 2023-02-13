64ed72b1ccbccb9816aada593ef2dfd975633249
Reiny Bo <reinybotros@Reinys-MacBook-Pro.local>
2023-02-12 21:53:55 -0800

    Config deployment

--- diary/settings.py
+++ diary/settings.py
@@ -133,5 +133,5 @@ LOGOUT_REDIRECT_URL = '/'
 
 
 # Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+import django_on_heroku
+django_on_heroku.settings(locals())
