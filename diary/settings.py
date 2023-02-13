844867419e724218e653f59b8c728e8ecdab5e1e
Reiny Bo <reinybotros@Reinys-MacBook-Pro.local>
2023-02-12 20:47:00 -0800

    Config deployment3

--- diary/settings.py
+++ diary/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main_app',
+    'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
