cbb3e7bc64a03d964a47c3349e3f622f97418531
Reiny Bo <reinybotros@Reinys-MacBook-Pro.local>
2023-02-15 09:24:59 -0800

    env variables

--- diary/settings.py
+++ diary/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import os
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sm8*tw+633p(2%6%q%#18hq@_x(^4!(mme1b+f*uwggy8$xgrw'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +78,7 @@ WSGI_APPLICATION = 'diary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-import os
-import environ
+
 
 DATABASES = {
     'default': {
